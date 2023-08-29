c71bce705d10cb955d8152e9c4cb04160a33481d
Vladyslav Roznatovskyi <morphey138@gmail.com>
2023-08-29 22:53:37 +0300

    Card component implementation

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Button = (props: Props) => {
+  const { children } = props;
+
+  return (
+        <button
+          type="button"
+          className="text-white bg-accent hover:shadow-custom duration-300
+          font-bold font-sans rounded-lg text-sm px-5 py-2.5
+          dark:bg-blue-600 dark:hover:bg-blue-700 border border-accent w-full"
+        >
+          {children}
+        </button>
+  );
+};
