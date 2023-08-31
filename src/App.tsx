5de94682ec041e6d8ee2f3cecf0f232c3a63eb4a
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-08-31 18:38:03 +0300

    added router and styles from tw.config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 
-const navLinks = ['Home', 'Phones', 'Tablets', 'Accessories'];
+const navLinks = [
+  { name: 'Home', path: '/' },
+  { name: 'Phones', path: 'phones' },
+  { name: 'Tablets', path: 'tablets' },
+  { name: 'Accessories', path: 'accessories' },
+];
 
 const App = () => {
   return (
     <div>
-      <Header navLinks={navLinks} />
+      <Header links={navLinks} />
 
       <div className="p-4">
         <Outlet />
