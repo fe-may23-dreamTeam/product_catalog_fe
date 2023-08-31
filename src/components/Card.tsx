a23d6c78633ed290bf539f58b53d6e3382d41107
Vladyslav Roznatovskyi <morphey138@gmail.com>
2023-08-31 14:08:23 +0300

    add temporary phone info

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,10 +2,11 @@
 import { Button } from './Button';
 import { ProductProperties } from './ProductProperties';
 import { PhoneInfo } from '../App';
-import favoritesSVG from '../assets/icons/Favourites (Heart Like).svg';
+import emptyHeartIcon from '../assets/icons/empty-heart_icon.svg';
 // eslint-disable-next-line max-len
-import favoritesFilledSVG from '../assets/icons/Favourites Filled (Heart Like).svg';
+import filledHeartIcon from '../assets/icons/filled-heart_icon.svg';
 import { useState } from 'react';
+import { FiHeart } from 'react-icons/fi';
 
 type CardProps = {
   phoneInfo: PhoneInfo;
@@ -31,51 +32,43 @@ export const Card = (props: CardProps) => {
 
   return (
     <section
-      className="container box-border rounded-lg p-8 max-w-xs border
-    border-grey bg-white"
+      className="card box-border rounded-lg p-8 max-w-xs border
+    border-secondary bg-white"
     >
-      <div className="grid auto-rows-auto gap-y-2">
-        <img className="h-32 mx-auto" src={phoneImage} alt="phone-image" />
+      <div className="grid auto-rows-auto gap-y-2 object-cover">
+        <img className="mx-auto" src={phoneImage} alt="phone-image" />
         <h3 className="text-sm font-semibold mt-4">{phoneTitle}</h3>
         <div className="flex gap-2">
           <h3 className="text-xl font-extrabold leading-8">
             {discountPrice}
           </h3>
           <h3 className="text-xl font-semibold leading-8 line-through
-          text-grey_secondary decoration-from-font"
+          text-secondary decoration-from-font"
           >
             {phonePrice}
           </h3>
         </div>
-        <span className="border border-grey border-t w-full" />
+        <span className="border border-secondary border-t w-full" />
         <ProductProperties properties={phoneProperties} />
         <div className="flex justify-between gap-x-[8px]">
           <Button>Add to cart</Button>
           {favorite ? (
             <button
-              className="w-10 h-10 rounded-full border border-grey_icons
-            hover:border-grey_primary hover:scale-110
+              className="w-10 h-10 rounded-full border border-icons
+            hover:border-primary hover:scale-110
             flex justify-center items-center shrink-0 duration-300"
               onClick={handleClick}
             >
-              <img
-                src={favoritesSVG}
-                alt="heart-like-icon"
-                className="px-3 py-3"
-              />
+              <FiHeart className="text-secondary-accent"/>
             </button>
           ) : (
             <button
-              className="w-[40px] h-[40px] rounded-full border border-grey_icons
-            hover:border-grey_primary hover:scale-110
+              className="w-[40px] h-[40px] rounded-full border border-icons
+            hover:border-primary hover:scale-110
             flex justify-center items-center shrink-0 duration-300"
               onClick={handleClick}
             >
-              <img
-                src={favoritesFilledSVG}
-                alt="heart-like-icon"
-                className="px-3 py-3"
-              />
+              <FiHeart />
             </button>
           )}
         </div>
