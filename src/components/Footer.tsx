32d984fb04ad1fa63db2e8c8a1e40dcedb45f42e
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-08-31 21:46:59 +0300

    adding nav, react-icons, styles from tw.config

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,46 +1,54 @@
 /* eslint-disable max-len */
 import logo from '../assets/images/logo.png';
-import defaultSliderUp from '../assets/images/default-slider-up-icon.svg';
+import { FiChevronUp } from 'react-icons/fi';
 
 export const Footer = () => {
   return (
-    <footer className="border-t border-gray-300 h-48 sm:h-16 xl:px-38">
-      <div className="flex flex-col px-4 py-8 sm:flex-row sm:px-6 lg:px-32 sm:justify-between">
-        <div className="flex flex-col sm:flex-row sm:space-x-28 xl:space-x-72 sm:justify-between">
+    <footer className="border-t border-elements h-48 tablet:h-16 desktop:px-38 box-border">
+      <div className="flex flex-col px-4 py-8 tablet:flex-row tablet:px-6 tablet:justify-around">
+        <div className="flex flex-col tablet:flex-row tablet:space-x-32 tablet:justify-between tablet:items-center">
           <a href="">
-            <img className="flex" src={logo} alt="Nice Gadgets Logo" />
+            <img className="flex min-w-fit" src={logo} alt="Nice Gadgets Logo" />
           </a>
 
-          <div className="flex flex-col gap-4 sm:gap-0 mt-4 sm:flex-row sm:space-x-4 lg:space-x-32 sm:items-center">
-            <a
-              className="uppercase text-gray-400 font-extrabold text-xs hover:text-gray-900"
-              href="#"
-            >
-              Github
-            </a>
+          <nav>
+            <ul className="flex flex-col gap-4 mt-4 tablet:mt-0 tablet:gap-0 tablet:ml-12 tablet:flex-row tablet:space-x-4 desktop:space-x-32 tablet:justify-between desktop:ml-52">
+              <li>
+                <a
+                  className="uppercase text-secondary font-extrabold text-xs hover:text-primary active:text-primary focus:text-primary font-Mont"
+                  href="#"
+                >
+                  Github
+                </a>
+              </li>
 
-            <a
-              className="uppercase text-gray-400 font-extrabold text-xs hover:text-gray-900"
-              href="#"
-            >
-              Contacts
-            </a>
+              <li>
+                <a
+                  className="uppercase text-secondary font-extrabold text-xs hover:text-primary active:text-primary focus:text-primary font-Mont"
+                  href="#"
+                >
+                  Contacts
+                </a>
+              </li>
 
-            <a
-              className="uppercase text-gray-400 font-extrabold text-xs hover:text-gray-900"
-              href="#"
-            >
-              Rights
-            </a>
-          </div>
+              <li>
+                <a
+                  className="uppercase text-secondary font-extrabold text-xs hover:text-primary active:text-primary focus:text-primary font-Mont"
+                  href="#"
+                >
+                  Rights
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
 
-        <div className="flex justify-center sm:justify-center items-center space-x-4 mt-4 sm:mt-0">
-          <p className=" text-gray-400 font-bold text-xs whitespace-nowrap  hover:text-gray-900">
+        <div className="flex justify-center tablet:justify-center items-center space-x-4 mt-4 tablet:mt-0">
+          <p className=" text-secondary font-bold text-xs whitespace-nowrap tablet:pl-24">
             Back to top
           </p>
-          <div className="border rounded-full border-gray-400 border-solid hover:border-gray-900 h-8 w-8">
-            <img className="p-2" src={defaultSliderUp} alt="" />
+          <div className="border rounded-full border-icons border-solid hover:border-primary active:text-primary focus:text-primary h-8 w-8 flex items-center justify-center">
+            <FiChevronUp className='w-4 h-4'/>
           </div>
         </div>
       </div>
