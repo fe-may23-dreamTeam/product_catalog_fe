c4394406ee5a7a5133c88fc4cc6ae7ab3d8672e5
NazarBaraban <barabannazar44@gmail.com>
2023-08-31 22:09:58 +0300

    fix sizes

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,14 +2,14 @@ import '../index.css';
 
 const NotFound = () => {
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 tablet:py-32 desktop:px-8">
       <div className="text-center">
         <img
           src={require('../assets/404-error.png')}
           alt="404-error-image"
           className="mx-auto w-[250px] mb-4 mt-[-100px]"
         />
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-primary sm:text-5xl">
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-primary tablet:text-5xl">
           Page not found
         </h1>
         <p className="mt-6 text-base leading-7 text-secondary">
