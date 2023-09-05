1ddcdf4e2cc8e1a4a31b84f3589d8e79c44646b7
Max Kuznetsov <kuznetsov.max.v@gmail.com>
2023-09-05 17:48:34 +0300

    refactoring

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -4,6 +4,7 @@ import CartItem from '../components/CartItem';
 import { getTotalProductsCost } from '../utils/getTotalCost';
 import { getTotalItemsCount } from '../utils/getTotalItemsCount';
 import { useAppSelector } from '../redux';
+import { NavLink } from 'react-router-dom';
 
 export const CartPage: React.FC = () => {
   const { items: cartItems } = useAppSelector((state) => state.cart);
@@ -19,24 +20,33 @@ export const CartPage: React.FC = () => {
         </h1>
       </div>
 
-      <div className="flex flex-col desktop:flex-row justify-between desktop:items-start gap-4">
-        <div className="flex flex-col gap-4">
-          {cartItems.map((item) => (
-            <CartItem key={item.id} item={item} />
-          ))}
-        </div>
-
-        <div className="box-border flex flex-col mx-auto desktop:mx-0 items-center rounded-lg p-6 w-full border border-elements bg-hover-bg desktop:w-[368px]">
-          <h3 className="text-center text-primary text-[32px] font-extrabold leading-[41px]">
-            {`${totalCost}$`}
-          </h3>
-          <div className="text-center text-secondary text-sm font-semibold leading-[21px] mb-4">
-            {`Total for ${totalItemsCount} items`}
+      {cartItems.length ? (
+        <div className="flex flex-col desktop:flex-row justify-between desktop:items-start gap-4">
+          <div className="flex flex-col gap-4">
+            {cartItems.map((item) => (
+              <CartItem key={item.id} item={item} />
+            ))}
           </div>
-          <span className="w-full h-[0px] border text-elements mb-4"></span>
-          <Button>Checkout</Button>
+
+          <div className="box-border flex flex-col mx-auto desktop:mx-0 items-center rounded-lg p-6 w-full border border-elements bg-hover-bg desktop:w-[368px]">
+            <h3 className="text-center text-primary text-[32px] font-extrabold leading-[41px]">
+              {`${totalCost}$`}
+            </h3>
+            <div className="text-center text-secondary text-sm font-semibold leading-[21px] mb-4">
+              {`Total for ${totalItemsCount} items`}
+            </div>
+            <span className="w-full h-[0px] border text-elements mb-4"></span>
+            <Button>Checkout</Button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <h3 className="mb-2">Your cart is empty</h3>
+          <NavLink to="/phones" className="cursor-pointer font-bold w-fit">
+            Start shopping now!
+          </NavLink>
+        </>
+      )}
     </main>
   );
 };
