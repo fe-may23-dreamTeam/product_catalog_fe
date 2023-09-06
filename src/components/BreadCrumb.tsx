0ea5793cfdd7642cd858c9a0acac7379aa7dc122
Max Kuznetsov <kuznetsov.max.v@gmail.com>
2023-09-06 09:10:20 +0300

    breadcrumb v1

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
 import { FiArrowRight, FiHome } from 'react-icons/fi';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const BreadCrumb = () => {
+  const { pathname } = useLocation();
+
+  const routes = pathname.split('/').splice(1);
+
   return (
-    <div className="flex items-center col-span-4 tablet:col-span-12 desktop:col-span-24 mt-[25px]">
-      <FiHome />
-      <FiArrowRight className="ml-2" />
-      <p className="ml-2 text-xs">Phones</p>
-      <FiArrowRight className="ml-2" />
-      <p className="ml-2 text-xs">
-        Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)
-      </p>
+    <div className="flex items-center justify-self-start mb-6 col-span-4 tablet:col-span-12 desktop:col-span-24 mt-6">
+      <NavLink to="/">
+        <FiHome />
+      </NavLink>
+      {routes.map((route, i) => (
+        <>
+          <FiArrowRight className="ml-2" />
+          <NavLink to={`/${route}`} className="ml-2 text-xs">
+            {route}
+          </NavLink>
+        </>
+      ))}
     </div>
   );
 };
