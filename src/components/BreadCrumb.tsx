db6f2ba176c649e4c3921575b81ac6cbe3c4bc7f
Max Kuznetsov <kuznetsov.max.v@gmail.com>
2023-09-06 13:55:38 +0300

    breadcrumb v2

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -1,23 +1,23 @@
-import { FiArrowRight, FiHome } from 'react-icons/fi';
-import { NavLink, useLocation } from 'react-router-dom';
+import React from 'react';
+import { FiChevronRight, FiHome } from 'react-icons/fi';
+import { NavLink } from 'react-router-dom';
+import useBreadcrumbs from 'use-react-router-breadcrumbs';
 
 const BreadCrumb = () => {
-  const { pathname } = useLocation();
-
-  const routes = pathname.split('/').splice(1);
+  const breadcrumbs = useBreadcrumbs();
 
   return (
     <div className="flex items-center justify-self-start mb-6 col-span-4 tablet:col-span-12 desktop:col-span-24 mt-6">
       <NavLink to="/">
         <FiHome />
       </NavLink>
-      {routes.map((route, i) => (
-        <>
-          <FiArrowRight className="ml-2" />
-          <NavLink to={`/${route}`} className="ml-2 text-xs">
-            {route}
+      {breadcrumbs.splice(1).map(({ match, breadcrumb }) => (
+        <React.Fragment key={match.pathname}>
+          <FiChevronRight className="ml-2" />
+          <NavLink to={match.pathname} className="ml-2 text-xs">
+            {breadcrumb}
           </NavLink>
-        </>
+        </React.Fragment>
       ))}
     </div>
   );
