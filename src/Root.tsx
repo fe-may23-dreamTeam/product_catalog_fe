751b8b66fb69586516a730c90e9d13b90ff54c70
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-09-07 22:59:50 +0300

    error investigation

--- src/Root.tsx
+++ src/Root.tsx
@@ -25,7 +25,7 @@ export const Root = () => (
         <Route path="home" element={<Navigate to="/" replace />} />
         <Route path="phones" element={<Outlet />}>
           <Route index element={<CatalogPage />} />
-          <Route path=":phoneId?" element={<ProductPage />} />
+          <Route path=":product?" element={<ProductPage />} />
         </Route>
         <Route path="tablets">
           <Route path=":tabletId?" element={<CatalogPage />} />
