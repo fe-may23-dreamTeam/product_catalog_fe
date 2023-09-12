3ddb4af14e0c6cde6b0357f6254cf99a7350989a
Olena <pampam32121@gmail.com>
2023-09-13 01:12:46 +0300

    add ContactCard component with data

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,4 +1,6 @@
 import BreadCrumb from '../components/BreadCrumb';
+import ContactCard from '../components/ContactCard';
+import { contacts } from '../utils/contacts';
 
 const ContactsPage = () => {
   return (
@@ -15,60 +17,9 @@ const ContactsPage = () => {
       </div>
 
       <section className="grid grid-cols-1 gap-4 tablet:grid-cols-2 desktop:grid-cols-4">
-        <article
-          className="card box-border p-8 relative
-            border border-secondary rounded-lg
-            min-w-[272px]  max-h-[440px]
-            bg-white
-            hover:shadow-card tablet:max-h-[506px]"
-        >
-          <h2>Olena Vats</h2>
-        </article>
-        <article
-          className="card box-border p-8 relative
-            border border-secondary rounded-lg
-            min-w-[272px]  max-h-[440px]
-            bg-white
-            hover:shadow-card tablet:max-h-[506px]"
-        >
-          <h2>Olena Vats</h2>
-        </article>
-        <article
-          className="card box-border p-8 relative
-            border border-secondary rounded-lg
-            min-w-[272px]  max-h-[440px]
-            bg-white
-            hover:shadow-card tablet:max-h-[506px]"
-        >
-          <h2>Olena Vats</h2>
-        </article>
-        <article
-          className="card box-border p-8 relative
-            border border-secondary rounded-lg
-            min-w-[272px]  max-h-[440px]
-            bg-white
-            hover:shadow-card tablet:max-h-[506px]"
-        >
-          <h2>Olena Vats</h2>
-        </article>
-        <article
-          className="card box-border p-8 relative
-            border border-secondary rounded-lg
-            min-w-[272px]  max-h-[440px]
-            bg-white
-            hover:shadow-card tablet:max-h-[506px]"
-        >
-          <h2>Olena Vats</h2>
-        </article>
-        <article
-          className="card box-border p-8 relative
-            border border-secondary rounded-lg
-            min-w-[272px]  max-h-[440px]
-            bg-white
-            hover:shadow-card tablet:max-h-[506px]"
-        >
-          <h2>Olena Vats</h2>
-        </article>
+        {contacts.map((contact) => (
+          <ContactCard key={contact.id} contact={contact} />
+        ))}
       </section>
     </main>
   );
